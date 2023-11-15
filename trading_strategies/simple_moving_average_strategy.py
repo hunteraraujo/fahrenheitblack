ac724b20157189c3cf5bcc4ccfc98e324470ffc7
hunteraraujo <hunter_araujo@msn.com>
2023-11-14 19:21:45 -0800

    refactor: Refactor abstract class and example strategy to leverage new signal data model
    - Refactored the `StrategyInterface` abstract base class and the `SimpleMovingAverageStrategy` class to accommodate the new `StockSignal` data model.
    - Updated the `analyze` method in the `SimpleMovingAverageStrategy` class to generate `StockSignal` instances based on the crossover of the simple moving average.
    - The `signals` attribute in the `StrategyInterface` class now holds a list of trading signals instead of a Series.
    - Incorporates the new signal data model for improved code readability and maintainability.

--- trading_strategies/simple_moving_average_strategy.py
+++ trading_strategies/simple_moving_average_strategy.py
@@ -1,23 +1,40 @@
-from pandas import DataFrame, Series
-
+from entities.stock_signal import StockSignal
 from strategy_interface import StrategyInterface
+from pandas import DataFrame
 
 class SimpleMovingAverageStrategy(StrategyInterface):
     """
     A simple moving average crossover strategy implementation.
+    
+    This strategy generates buy or sell signals based on the crossover of the simple 
+    moving average. A buy signal is generated when the current SMA value crosses above 
+    the previous SMA value, and a sell signal when it crosses below.
     """
 
     def analyze(self, data: DataFrame):
         """
-        Analyzes the market data and generates trading signals based on a simple moving average crossover strategy.
+        Analyzes the market data and generates trading signals based on a simple moving 
+        average crossover strategy.
         
         Parameters:
-            data (DataFrame): The most recent market data to analyze.
+            data (DataFrame): The most recent market data to analyze. It is assumed 
+            that the DataFrame contains a 'Close' column for closing prices and an 'SMA' 
+            column for simple moving average values.
         """
-        # Assume data contains 'SMA' column with the simple moving average values
+        self.signals = []
+
+        # Example parameters for signal generation
+        symbol = "AAPL"  # Example stock symbol
+        quantity = 100  # Example quantity
+        order_type = "MARKET"  # or "LIMIT"
+        price = data['Close'][-1]  # Current closing price, for example
+
+        # Signal generation logic
         if data['SMA'][-1] > data['SMA'][-2]:
-            # Create a Series of 1's with the same index as the data indicating a "buy" signal
-            self.signals = Series(1, index=data.index)
+            signal_type = "BUY"
+            signal = StockSignal(symbol, signal_type, quantity, order_type, price)
+            self.signals.append(signal)
         else:
-            # Create a Series of -1's with the same index as the data indicating a "sell" signal
-            self.signals = Series(-1, index=data.index)
+            signal_type = "SELL"
+            signal = StockSignal(symbol, signal_type, quantity, order_type, price)
+            self.signals.append(signal)
